a8ebd4274ddfbaaee2d4a97a69ae59a4a0b79e0d
CloudKMonet <98986434+CloudKMonet@users.noreply.github.com>
2025-08-01 17:35:25 -0400

    Add files via upload
    Cute mini app idea based on the saying "Counting Sheep". I don't really have plans to revisit it. But that could change- especially if I decide to make more simple console apps based on a saying.

---
+++ CountingSheep.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//This was a really fun mini project- I really wanted to do a "Counting sheep" as an example of how to utilize a loop!
+
+namespace Multiple_New_Projects_and_Revamps.New_Projects
+{
+    internal class CountingSheep
+    {
+        static void Main()
+        {
+            
+            WriteLine("Do you count sheep before you go to sleep? (Type: yes/no)");
+            /* we label this with sring response, because with boolean variables, we cannot use ints so no typying
+            yes or no! */
+            string response = ReadLine().ToLower();
+
+            //here is the boolean that the if statement is operating off of.
+            if (response == "yes")
+            {
+                WriteLine("How many sheep do you count? (please input a number)");
+                int sheepCount = Convert.ToInt32(Console.ReadLine());
+
+                /*Here is our loop. The i = 1 stands for the input
+                  i <= sheepCount means the numeber is less than or equal to the sheep count
+                  i++ is a nice way to exit the loop after fulfilling the criteria inside. Which is why I used a thread.
+                  the thread simulates a person pausing to count "sheep". 
+                 */
+                for (int i = 1; i <= sheepCount; i++) 
+                {
+                    WriteLine($"{i} sheep...");
+                    //Pause for 0.5 seconds to simulate a person counting sheep.
+                    Thread.Sleep(500);
+                }
+
+                WriteLine("Zzz... zzz....");
+            }
+            // if you input a no, the app will just end. 
+            else
+            {
+                WriteLine("Oh! Sorry- this app isn't for you then :(");
+            }
+            
+        }
+    }
+}
