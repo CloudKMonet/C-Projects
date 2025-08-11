1477072a3efd42d8d65ca73c6bc5b07a50d48abc
CloudKMonet <98986434+CloudKMonet@users.noreply.github.com>
2025-08-10 22:01:01 -0400

    Update CountingSheep.cs
    Made the comments a bit more professional in tone, and put a reminder in comments to add a switch case for types of sheep that the user can choose to count before sleep.

--- CountingSheep.cs
+++ CountingSheep.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 //This was a really fun mini project- I really wanted to do a "Counting sheep" as an example of how to utilize a loop!
+//Possible Future addition: Would be like a switch case in the beginning that would ask the user- what type of sheep before sleep?
 
 namespace Multiple_New_Projects_and_Revamps.New_Projects
 {
@@ -15,20 +16,20 @@ namespace Multiple_New_Projects_and_Revamps.New_Projects
         {
             
             WriteLine("Do you count sheep before you go to sleep? (Type: yes/no)");
-            /* we label this with sring response, because with boolean variables, we cannot use ints so no typying
+            /* I'll label this with string response, because with boolean variables, we cannot use ints. So no typying
             yes or no! */
             string response = ReadLine().ToLower();
 
-            //here is the boolean that the if statement is operating off of.
+            //Here is the boolean that the if statement is operating off of.
             if (response == "yes")
             {
                 WriteLine("How many sheep do you count? (please input a number)");
                 int sheepCount = Convert.ToInt32(Console.ReadLine());
 
-                /*Here is our loop. The i = 1 stands for the input
-                  i <= sheepCount means the numeber is less than or equal to the sheep count
+                /*Here is our loop. The i = 1 stands for the input.
+                  i <= sheepCount means the numeber is less than or equal to the sheep count.
                   i++ is a nice way to exit the loop after fulfilling the criteria inside. Which is why I used a thread.
-                  the thread simulates a person pausing to count "sheep". 
+                  The thread simulates a person pausing to count "sheep" jumping/ or galloping in and out of view. 
                  */
                 for (int i = 1; i <= sheepCount; i++) 
                 {
@@ -39,7 +40,7 @@ namespace Multiple_New_Projects_and_Revamps.New_Projects
 
                 WriteLine("Zzz... zzz....");
             }
-            // if you input a no, the app will just end. 
+            //If you input a no, the app will just end. 
             else
             {
                 WriteLine("Oh! Sorry- this app isn't for you then :(");
@@ -48,3 +49,4 @@ namespace Multiple_New_Projects_and_Revamps.New_Projects
         }
     }
 }
+
