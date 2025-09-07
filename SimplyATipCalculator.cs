5a0c20f2e38bae32addaee44b70e63144af7dcce
CloudKMonet <98986434+CloudKMonet@users.noreply.github.com>
2025-09-06 20:12:40 -0400

    Add files via upload
    A Simple Tip Calculator is now added to my small rotation of mini console apps!! :D

---
+++ SimplyATipCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiple_New_Projects_and_Revamps.New_Projects
+{
+    internal class SimplyATipCalculator
+    {
+        static void Main()
+        {
+            //Welcome Message
+            WriteLine("Welcome to the Simple Tip Calculator! :D ");
+
+            //Convert the bill amount and tip percentage to doubles to allow for decimal points.
+            WriteLine("What was the total amount for your bill? (Please no $ sign, just a number!)");
+            double billAmount = Convert.ToDouble(ReadLine());
+            WriteLine("What percentage would you like to tip? (Please a input number, no % sign!)");
+            double tipPercentage = Convert.ToDouble(ReadLine());
+
+            //Calculates the tip amount and display it to the user; it is also formatted into two decimal places.
+            double tipAmount = billAmount * (tipPercentage / 100);
+            WriteLine($"You should leave a tip of about: ${tipAmount:F2}");
+            WriteLine("Thank you for using the Simple Tip Calculator! :D");
+
+            //Optional for the user to run the program again or exit.
+            WriteLine("Press 'A' for again, and 'E' to exit.");
+            string response = ReadLine().ToLower();
+            if (response == "a")
+            {
+                Main();
+            }
+            else
+            {
+            //Environement Exit is used to close the program.
+                Environment.Exit(0);
+            }
+        }
+    }
+}
